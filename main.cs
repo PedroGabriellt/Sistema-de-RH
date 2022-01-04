1a77eeebf7e97631ad861996c0ab54e7c1de5f29
pedro gabriel <93726856+PedroGabriellt@users.noreply.github.com>
2022-01-04 23:33:16 +0000

    Criado duas classes e dois construtores

--- main.cs
+++ main.cs
@@ -1,12 +1,39 @@
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+class Gertor {
+
+  private int id;
+  private string area;
+  private string nome;
+
+  public Gestor(int id, string area, string nome){
+    this.id = id;
+    this.area = area;
+    this.nome = nome;
+  }
+
+}
+
+class Solicitação{
+
+  private int id;
+  private string AreaDeAtuação;
+  private int Qfuncionarios;
+  private string Turno;
+  private string Resposta;
+  private string Aprovados;
+
+  public Solicitação(int id, string AreaDeAtuação, int Qfuncionarios, string Turno, string Resposta, string Aprovados){
+
+  this.id = id;
+  this.AreaDeAtuação = AreaDeAtuação;
+  this.Qfuncionarios = Qfuncionarios > 0 ? Qfuncionarios : 0;
+  this.Turno = Turno;
+  this.Resposta = Resposta;
+  this.Aprovados = Aprovados;
+
+
+
   }
 }
 
-class Gertor {
-
-  
-}
