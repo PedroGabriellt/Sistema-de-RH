bd9f958a9da37213984b1e73d74a09bdbab595ec
pedro gabriel <93726856+PedroGabriellt@users.noreply.github.com>
2022-01-05 22:33:10 +0000

    Classes criadas.

--- main.cs
+++ main.cs
@@ -37,3 +37,44 @@ class Solicitação{
   }
 }
 
+
+class FuncionarioDoRH {
+ private int id;
+ private string Nome;
+
+ public FuncionarioDoRH(int id, string Nome){
+   this.id = id;
+   this.Nome = Nome;
+
+ }
+}
+
+
+class Curricolo {
+  private int id;
+  private string Formacao;
+  private string Experiencia;
+
+  public Curricolo( int id, string Formacao, string Experiencia){
+    this.id = id;
+    this.Formacao = Formacao;
+    this.Experiencia = Experiencia;
+  }
+
+}
+
+class Candidato{
+  private int id;
+  private string Nome;
+  private int idade;
+  private string Telefone;
+  private string Email;
+
+  public Candidato(int id, string Nome, int idade, string Telefone, string Email){
+    this.id = id;
+    this.Nome = Nome;
+    this.idade = idade;
+    this.Telefone = Telefone;
+    this.Email = Email;
+  }
+}
